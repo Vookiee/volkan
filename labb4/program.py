7b2f0f50f1f5ca9f386891ca7c9db97fb7d125a8
Volkan <mrvolkan163@gmail.com>
2020-02-20 11:25:50 +0100

    abc

--- labb4/program.py
+++ labb4/program.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
 from flask import request
+import random
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,7 +9,7 @@ def index():
 
 @app.route('/', methods=['POST'])
 def register():
-    killer = [
+    info = [
     request.form.get('fname'),
     request.form.get('ename'),
     request.form.get('address'),
@@ -22,19 +23,41 @@ def register():
     request.form.get('format'),
     request.form.get('comment')]
 
-    f_name = killer[0]
-    e_name = killer[1]
-    address = killer[2]
-    postnmr = killer[3]
-    stad = killer[4]
-    telnmr = killer[5]
-    email = killer[6]
-    losenord = killer[7]
-    betala = killer[8]
-    offer = killer[9]
-    form = killer[10]
-    kommentar = killer[11]
+    f_name = info[0]
+    e_name = info[1]
+    address = info[2]
+    postnmr = info[3]
+    stad = info[4]
+    telnmr = info[5]
+    email = info[6]
+    losenord = info[7]
+    betala = info[8]
+    offer = info[9]
+    form = info[10]
+    kommentar = info[11]
     return render_template('index.html', fname = f_name, ename =e_name, address=address, postnmr =postnmr, stad=stad, tele=telnmr, email=email,pw=losenord, pay=betala, offer=offer, for_mat=form, kommentar=kommentar)
 
-if __name__ == '__main__':
+@app.route('/')
+def postdata():
+    v = randint(1,100)
+    conn = sqlite3.connect('databas.db')
+    c = conn.cursor()
+    #c.execute("""CREATE TABLE data VALUES (rng integer)""")
+    c.execute('INSERT INTO data VALUES (v)')
+    conn.commit()
+    return data
+
+@app.route('/', ['GET'])
+def postdata():
+      #r = requests.post('https://en7w1brogvxyv.x.pipedream.net', json=data_dict)
+      conn = sqlite3.connect('databas.db')
+      c = conn.cursor()
+      c.execute('SELECT * FROM data')
+      data = c.fetchall()
+      conn.commit()
+      return justify(data)
+
+
+if __name__ == '__main__': 
     app.run()
+    
