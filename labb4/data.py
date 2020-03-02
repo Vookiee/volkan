8182f1deda2ca7cff8ef8fe3aa82027eacfc7a14
Volkan <mrvolkan163@gmail.com>
2020-03-02 20:17:16 +0100

    abcc

--- labb4/data.py
+++ labb4/data.py
@@ -24,25 +24,21 @@ def postJsonHandler():
     return '{"message":"JSON post success"}'
 
 
-# @app.route('/hej', methods = ['POST'])
-# def getnum():
-#     svar = requests.get('http://localhost:5050/rng')
-#     data = svar.json()
-#     conn = sqlite3.connect('databas.db')
-#     c = conn.cursor()
-#     c.execute("""CREATE TABLE volkan(rng, rng1, rng2)""")
-#     c.execute('INSERT INTO volkan (rng, rng1, rng2) VALUES (?,?,?)',data)
-#     #c.execute('SELECT * FROM volkan')
-#     c.execute('SELECT rng FROM volkan')
-#     rng = c.fetchall()
-#     c.execute('SELECT rng1 FROM volkan')
-#     rng1 = c.fetchall()
-#     c.execute('SELECT rng2 FROM volkan')
-#     rng2 = c.fetchall()
-#     #print(tables[0])
-#     conn.commit()
-#     conn.close()
-#     return render_template('databas.html', rng = rng, rng1 = rng1, rng2 = rng2)
+@app.route('/hej')
+def getnum():
+    conn = sqlite3.connect('dats.db')
+    c = conn.cursor()
+    c.execute('SELECT rng FROM volkan')
+    rng = c.fetchall()
+    c.execute('SELECT rng1 FROM volkan')
+    rng1 = c.fetchall()
+    c.execute('SELECT rng2 FROM volkan')
+    rng2 = c.fetchall()
+    
+    conn.commit()
+    conn.close()
+    return render_template('databas.html',rng = rng, rng1 = rng1, rng2=rng2)
+
 
 if __name__ == '__main__': 
     app.run(host='0.0.0.0', port=5050)
--- labb4/rng.py
+++ labb4/rng.py
@@ -4,14 +4,12 @@ from random import randint
 import time
 import json
 
+while True:
+    time.sleep(5)
+    rng = randint(10,200)
+    rng1 = randint(300,1000)
+    rng2 = randint(5,10)
+    s = {'rng':rng,'rng1':rng1, 'rng2':rng2}
 
-time.sleep(5)
-rng = randint(10,200)
-rng1 = randint(300,1000)
-rng2 = randint(5,10)
-s = {'rng':rng,'rng1':rng1, 'rng2':rng2}
-
-r = requests.post('http://localhost:5050/rng', json = s)
-print(r.status_code)
-print(r.json())
-print(r.headers)
+    r = requests.post('http://localhost:5050/rng', json = s)
+    time.sleep(10)
